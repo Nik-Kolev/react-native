d1981342af6e235e99f60de1592a5a0ec415f7cc
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-08-27 22:49:39 +0300

    Implemented clerk with google oauth

--- pets/app/index.jsx
+++ pets/app/index.jsx
@@ -1,7 +1,21 @@
-import { Link } from 'expo-router';
+import { useUser } from '@clerk/clerk-expo';
+import { Link, useRootNavigationState } from 'expo-router';
+import { useEffect } from 'react';
 import { Text, View } from 'react-native';
 
 export default function Index() {
+  const { user } = useUser();
+
+  // const rootNavigationState = useRootNavigationState();
+
+  // useEffect(() => {
+  //   checkNavLoaded();
+  // }, []);
+
+  // const checkNavLoaded = () => {
+  //   if (!rootNavigationState.key) return null;
+  // };
+
   return (
     <View
       style={{
