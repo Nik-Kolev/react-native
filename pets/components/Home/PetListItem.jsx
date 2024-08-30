48688d35874fbfb2fbb767938c6fce4845cae030
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-08-30 19:12:03 +0300

    fast changes

--- pets/components/Home/PetListItem.jsx
+++ pets/components/Home/PetListItem.jsx
@@ -1,10 +1,20 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import Colors from '../../constants/Colors';
+import { useRouter } from 'expo-router';
 
 export default function PetListItem({ pet }) {
+  const router = useRouter();
   return (
-    <View style={{ padding: 10, backgroundColor: Colors.WHITE, marginRight: 15, borderRadius: 10, borderColor: 'black', borderWidth: 1 }}>
+    <TouchableOpacity
+      style={{ padding: 10, backgroundColor: Colors.WHITE, marginRight: 15, borderRadius: 10, borderColor: 'black', borderWidth: 1 }}
+      onPress={() =>
+        router.push({
+          pathname: '/pet-details',
+          params: pet,
+        })
+      }
+    >
       <Image source={{ uri: pet?.imageUrl }} style={{ width: 150, height: 135, objectFit: 'cover', borderRadius: 10 }} />
       <Text style={{ fontFamily: 'outfit-medium', fontSize: 22, textAlign: 'center', marginTop: 5 }}>{pet?.name}</Text>
       <View style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row', marginTop: 10, alignItems: 'center' }}>
@@ -15,6 +25,6 @@ export default function PetListItem({ pet }) {
           {pet?.age} Yrs
         </Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
